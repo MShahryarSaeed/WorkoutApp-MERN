244128149c2fba7c6163470a4768cd073e2c0c49
MShahryarSaeed <shahryarsaeed06.com>
2024-04-30 22:29:07 +0500

    Change Backend URL

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -9,7 +9,7 @@ export const useLogin=()=>{
  
   const login=async(email,password)=>{
 
-    const response=await fetch(`/api/users/login`,{
+    const response=await fetch(`https://workout-app-mern-iota.vercel.app/api/users/login`,{
       method:'POST',
       body:JSON.stringify({email,password}),
       headers:{
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     const fetchWorkouts = async () => {
 
       try {
-        const response = await fetch(`/api/workouts`,{
+        const response = await fetch(`https://workout-app-mern-iota.vercel.app/api/workouts`,{
           headers:{
             'Authorization':`Bearer ${user.accessToken}`
           }
